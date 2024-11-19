fa582fa2ab1cc8f80785b601030f43b69946754a
guchur <kdjin11@naver.com>
2024-11-19 11:14:25 +0900

    [chore] metadata url

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ export const metadata: Metadata = {
         template: '%s | Dashboard', default: 'Dashboard by Next.js',
     },
     description: '리액트 프로그래밍 with Next.js - 대시보드 프로젝트',
-    metadataBase: new URL('https://dashboard.vercel.app/'),
+    metadataBase: new URL('https://levelup-dashboard.vercel.app/'),
 
 };
 
